a530cf5229288cbc377ca81a4da8007b72fdd632
Ricky <rzh4321@gmail.com>
2023-05-26 21:01:27 -0400

    created create_buttons which creates add task and remove project buttons

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const projs = [];
 let project_container = document.querySelector('.projects');
 let create_proj_btn = document.querySelector('#create-project button');
 let proj_name = document.querySelector('#create-project input');
+
 create_proj_btn.addEventListener('click', (e) => {
     let proj;
     if (proj_name.value == '') {
@@ -18,6 +19,8 @@ create_proj_btn.addEventListener('click', (e) => {
     create_empty_project(proj.name, proj.id);
 })
 
+
+
 function create_empty_project(name, id) {
     let container = document.createElement('div');
     container.classList.add('project');
@@ -26,5 +29,20 @@ function create_empty_project(name, id) {
     let title = document.createElement('span');
     title.classList.add('title');
     title.textContent = name;
-    container.append(title);
+    container.append(title, create_buttons());
+}
+
+function create_buttons() {
+    let btn_container = document.createElement('div');
+    btn_container.classList.add('btn-container');
+    let add_task = document.createElement('button');
+    add_task.textContent = "+ add task";
+    let remove_proj = document.createElement('button');
+    remove_proj.textContent = "- remove project";
+    remove_proj.style.backgroundColor = 'purple';
+    btn_container.append(add_task, remove_proj);
+
+    
+
+    return btn_container;
 }
