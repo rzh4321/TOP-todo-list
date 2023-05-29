19b4be712d9ee1949149f67d453702638f9221e2
Ricky <rzh4321@gmail.com>
2023-05-28 22:58:42 -0400

    deleted storage.js

--- src/storage.js
+++
@@ -1,9 +0,0 @@
-function get_ids() {
-    let projs = JSON.parse(localStorage.getItem('projs'));
-    // project has never been created before
-    if (projs == null || projs.length == 0) return [1, -1];
-    let proj_id = projs[projs.length - 1].id + 1
-    let task_id = projs[projs.length - 1].tasks[tasks.length - 1].id - 1
-    return [proj_id, task_id];
-}
-
